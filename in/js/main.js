db3c7fc352d1b112e9bb1fb7adaeb4bbe3ba2e70
Usama Husain <nhusain34@gmail.com>
2024-04-26 15:07:55 +0530

    Mobile Menu Error Corrected

--- in/js/main.js
+++ in/js/main.js
@@ -270,9 +270,26 @@ jQuery(document).ready(function($) {
 	siteSticky();
 
 	// navigation
+	// var OnePageNavigation = function() {
+	// 	var navToggler = $('.site-menu-toggle');
+	// 	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
+	// 		e.preventDefault();
+
+	// 		var hash = this.hash;
+
+	// 		$('html, body').animate({
+	// 			'scrollTop': $(hash).offset().top
+	// 		}, 600, 'easeInOutCirc', function(){
+	// 			window.location.hash = hash;
+	// 		});
+
+	// 	});
+	// };
+	// OnePageNavigation();
+	// Modified navigation
 	var OnePageNavigation = function() {
 		var navToggler = $('.site-menu-toggle');
-		$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
+		$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#']", function(e) {
 			e.preventDefault();
 
 			var hash = this.hash;
